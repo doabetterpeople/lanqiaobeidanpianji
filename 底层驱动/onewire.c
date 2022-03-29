698d0b222c037c5911541ef3b4b4c79f6848c2b6
doabetterpeople <1985866080@qq.com>
2022-03-29 20:34:01 +0800

    底层驱动

---
+++ 底层驱动/onewire.c
@@ -0,0 +1,73 @@
+#include "reg52.h"
+
+sbit DQ = P1^4;
+
+//单总线延时函数
+#ifndef STC12  
+void Delay_OneWire(unsigned int t)  //STC89C52RC
+{
+	while(t--);
+}
+#else
+void Delay_OneWire(unsigned int t)  //STC12C5260S2
+{
+	unsigned char i;
+	while(t--){
+		for(i=0;i<12;i++);
+	}
+}
+#endif
+
+//通过单总线向DS18B20写一个字节
+void Write_DS18B20(unsigned char dat)
+{
+	unsigned char i;
+	for(i=0;i<8;i++)
+	{
+		DQ = 0;
+		DQ = dat&0x01;
+		Delay_OneWire(50);
+		DQ = 1;
+		dat >>= 1;
+	}
+	Delay_OneWire(50);
+}
+
+//从DS18B20读取一个字节
+unsigned char Read_DS18B20(void)
+{
+	unsigned char i;
+	unsigned char dat;
+  
+	for(i=0;i<8;i++)
+	{
+		DQ = 0;
+		dat >>= 1;
+		DQ = 1;
+		if(DQ)
+		{
+			dat |= 0x80;
+		}	    
+		Delay_OneWire(50);
+	}
+	return dat;
+}
+
+//DS18B20初始化
+bit init_ds18b20(void)
+{
+  	bit initflag = 0;
+  	
+  	DQ = 1;
+  	Delay_OneWire(120);
+  	DQ = 0;
+  	Delay_OneWire(800); // 延时大于480us
+  	DQ = 1;
+  	Delay_OneWire(100);  // 14
+  	initflag = DQ;     // initflag等于1初始化失败
+  	Delay_OneWire(50);
+  
+  	return initflag;
+}
+
+
